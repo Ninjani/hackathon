80b850e2d34ae024581798406e2f2a2d1aa41a49
Jay <>
2023-05-18 15:27:16 +0200

    fix main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pytorch_lightning.cli import LightningCLI
 import torch
 import warnings
-warnings.filterwarnings('ignore', category=UserWarning, message='TypedStorage is deprecated')
+warnings.filterwarnings('ignore')
 
 def main():
     """
@@ -9,7 +9,7 @@ def main():
     Or in an sbatch script with srun python main.py fit -c config.yaml
     """
     torch.set_float32_matmul_precision('medium')
-    cli = LightningCLI()
+    cli = LightningCLI(save_config_kwargs={"overwrite": True})
 
 if __name__ == '__main__':
     main()
