25cf03d6fd676bc47646d6a6b2355704ac2cb191
Jay <48680156+Ninjani@users.noreply.github.com>
2023-05-23 20:51:42 +0200

    bugfixes (#12)

--- source/dataloader/pair_loader.py
+++ source/dataloader/pair_loader.py
@@ -3,11 +3,11 @@ import pickle
 from torch_geometric.data import Dataset, Data, HeteroData
 from torch_geometric.loader import DataLoader
 from pytorch_lightning import LightningDataModule
-from torch_geometric import transforms as T
 import torch
+from tqdm import tqdm
 from ..utils import get_protein_pair_names, graphein_to_pytorch_graph, load_protein_as_graph, read_interface_labels
 
-def make_hetero_graph(graph_1: Data, graph_2: Data, sasa_threshold=None):
+def make_hetero_graph(graph_1: Data, graph_2: Data, use_surface=False):
     """
     Combine two pytorch-geometric Data objects into a single HeteroData object.
     i.e data_1.x and data_2.x become data["protein_1"].x and data["protein_2"].x
@@ -35,18 +35,17 @@ def make_hetero_graph(graph_1: Data, graph_2: Data, sasa_threshold=None):
                 interacting_edge_index.append([i, j])
                 # Euclidean distance between the pos vectors of the two nodes
                 interacting_edge_distance.append(torch.norm(graph_1.pos[i] - graph_2.pos[j]))
-    graph["protein_1", "interacts", "protein_2"].edge_index = torch.tensor(interacting_edge_index).T
-    graph["protein_1", "interacts", "protein_2"].edge_attr = torch.tensor(interacting_edge_distance).unsqueeze(1)
+    if len(interacting_edge_index) > 0:
+        graph["protein_1", "interacts", "protein_2"].edge_index = torch.tensor(interacting_edge_index).T
+        graph["protein_1", "interacts", "protein_2"].edge_attr = torch.tensor(interacting_edge_distance).unsqueeze(1)
 
-    # add all vs. all edges between the two proteins (or those above sasa_threshold if given)
+    # add all vs. all edges between the two proteins (or those with rel_sasa > 20 if use_surface=True)
     edge_index_inter = []
     for i in range(graph_1.x.shape[0]):
         for j in range(graph_2.x.shape[0]):
-            if sasa_threshold is None:
+            if not use_surface or (graph_1.rel_sasa[i] > 20 and graph_2.rel_sasa[j] > 20):
                 edge_index_inter.append([i, j])
-            else:
-                if graph_1.sasa[i] > sasa_threshold and graph_2.sasa[j] > sasa_threshold:
-                    edge_index_inter.append([i, j])
+    assert len(edge_index_inter) > 0, "No residues with rel_sasa > 20 found in one or both proteins"
     graph["protein_1", "inter", "protein_2"].edge_index = torch.tensor(edge_index_inter).T
     graph["protein_2", "inter", "protein_1"].edge_index = torch.tensor(edge_index_inter).T.flip(0)
     return graph
@@ -55,17 +54,18 @@ class ProteinPairDataset(Dataset):
     """
     torch-geometric Dataset class for loading pairs of protein files as HeteroData objects.
     """
-    def __init__(self, root, pdb_dir, processed_dir_suffix: str,
+    def __init__(self, root, pdb_dir, pkl_dir, processed_dir_suffix: str,
                  node_attr_columns: list, edge_attr_columns: list, 
-                 edge_kinds: set, sasa_threshold: float, 
+                 edge_kinds: set, use_surface: bool, 
                  protein_pair_names: list, label_mapping: dict, 
                  pre_transform=None, transform=None):
         self.pdb_dir = Path(pdb_dir)
+        self.pkl_dir = Path(pkl_dir)
         self.processed_dir_suffix = processed_dir_suffix
         self.node_attr_columns = node_attr_columns
         self.edge_attr_columns = edge_attr_columns
         self.edge_kinds = edge_kinds
-        self.sasa_threshold = sasa_threshold
+        self.use_surface = use_surface
         self.protein_pair_names = protein_pair_names
         self.label_mapping = label_mapping
         self.protein_names = set()
@@ -77,6 +77,10 @@ class ProteinPairDataset(Dataset):
     @property
     def processed_dir(self) -> str:
         return str(Path(self.root) / f"processed_{self.processed_dir_suffix}")    
+
+    @property
+    def raw_dir(self) -> str:
+        return str(self.pkl_dir)
 
     @property
     def raw_file_names(self):
@@ -127,6 +131,8 @@ class ProteinPairDataset(Dataset):
     
     def get(self, idx):
         data = torch.load(self.processed_file_names[idx])
+        if len(data["protein_1", "interacts", "protein_2"].edge_index) == 0:
+            del data["protein_1", "interacts", "protein_2"]
         return data
     
 
@@ -138,14 +144,15 @@ class ProteinPairDataModule(LightningDataModule):
     - the batch size and number of workers for the DataLoader
     - the transforms to apply to the individual graphs (in pre-transform) and to the combined HeteroData object (in transform)
     """
-    def __init__(self, root, pdb_dir, processed_dir_suffix, node_attr_columns, edge_attr_columns, edge_kinds, sasa_threshold=None, batch_size: int = 32, num_workers: int = 1):
+    def __init__(self, root, pdb_dir, pkl_dir, processed_dir_suffix, node_attr_columns, edge_attr_columns, edge_kinds, use_surface=False, batch_size: int = 32, num_workers: int = 1):
         super().__init__()
         self.pdb_dir = pdb_dir
+        self.pkl_dir = pkl_dir
         self.processed_dir_suffix = processed_dir_suffix
         self.node_attr_columns = node_attr_columns
         self.edge_attr_columns = edge_attr_columns
         self.edge_kinds = edge_kinds
-        self.sasa_threshold = sasa_threshold
+        self.use_surface = use_surface
         self.train_file = Path(root) / "training.txt"
         self.val_file = Path(root) / "testing.txt"
         self.test_file = Path(root) / "testing.txt"
@@ -169,18 +176,22 @@ class ProteinPairDataModule(LightningDataModule):
         protein_pair_names = get_protein_pair_names(names_file, 
                                                     protein_pair_names_to_labels=protein_pair_names_to_labels, 
                                                     protein_pair_names_to_non_labels=protein_pair_names_to_non_labels)
-        return ProteinPairDataset(root=self.root, pdb_dir=self.pdb_dir, processed_dir_suffix=self.processed_dir_suffix,
+        protein_pair_names = [p for p in protein_pair_names if (Path(self.pkl_dir) / f"{p[0]}.pkl").exists() and (Path(self.pkl_dir) / f"{p[1]}.pkl").exists()]
+        return ProteinPairDataset(root=self.root, pdb_dir=self.pdb_dir, pkl_dir=self.pkl_dir,
+                                  processed_dir_suffix=self.processed_dir_suffix,
                                   node_attr_columns=self.node_attr_columns, edge_attr_columns=self.edge_attr_columns, edge_kinds=self.edge_kinds,
-                                  protein_pair_names=protein_pair_names, label_mapping=protein_pair_names_to_labels_combi, sasa_threshold=self.sasa_threshold,
+                                  protein_pair_names=protein_pair_names, label_mapping=protein_pair_names_to_labels_combi, use_surface=self.use_surface,
                                   pre_transform=self.pre_transform, transform=self.transform)
     
     def prepare_data(self):
-        self._create_dataset(self.full_list_file)
+        self._create_dataset(self.full_list_file, use_non_interacting=True)
 
     def setup(self, stage: str):
         if stage == "fit":
             self.train_dataset = self._create_dataset(self.train_file, use_non_interacting=True)
             self.val_dataset = self._create_dataset(self.val_file)
+            print(f"Number of training pairs: {len(self.train_dataset)}")
+            print(f"Number of validation pairs: {len(self.val_dataset)}")
         elif stage == "test":
             self.test_dataset = self._create_dataset(self.test_file)
 
