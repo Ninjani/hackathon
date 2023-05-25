79725d756f416496069fffe6ac2da2a921c45d86
Jay <>
2023-05-25 09:52:30 +0200

    modify sbatch file to allow for different outputs

--- sbatch_train.sh
+++ sbatch_train.sh
@@ -1,16 +1,24 @@
+# Run as ./sbatch_train.sh <job_name> <config_file>
+
+# Retrieve command name from the command line
+JOB_NAME=$1
+CONFIG_FILE=$2
+
+# Use a heredoc to create the script
+cat << EOF | sbatch
 #!/bin/bash
-#SBATCH --job-name=train_hackathon
+#SBATCH --job-name=hackathon_"$JOB_NAME"
 #SBATCH --nodes=1             
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 #SBATCH --partition=rtx8000,pascal
-#SBATCH --time=0-06:00:00
-#SBATCH --output=train_hackathon.out
-#SBATCH --qos=6hours
+#SBATCH --output="$JOB_NAME".out
+#SBATCH --qos=1day
 
 # activate conda env
 export PATH=$HOME/miniconda3/bin:$PATH
 source activate hackathon
 
-srun python main.py fit -c $1
+srun python main.py fit -c $CONFIG_FILE
+EOF
